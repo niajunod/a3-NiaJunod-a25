dbf0b2501330deb9eea0fa1ac2f31abe7cc726fe
Antonia Junod <niajunod@gmail.com>
2025-09-12 15:19:24 -0400

    color change

--- server.js
+++ server.js
@@ -18,6 +18,8 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static('public'));
+
 
 // MongoDB connection
 mongoose
